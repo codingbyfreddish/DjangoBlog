8953f8751611f4d80f7640b1c96909ee2d2dd7e5
Fredrik Gustafsson <freddish@hotmail.com>
2023-03-21 13:43:04 +0000

    install, set up summernote

--- blog/models.py
+++ blog/models.py
@@ -9,7 +9,8 @@ STATUS = ((0, "Draft"), (1, "Pub;ished"))
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts")
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='blog_posts')
     update_on = models.DateTimeField(auto_now=True)
     content = models.TextField()
     featured_image = CloudinaryField('image', default='placeholder')
@@ -29,3 +30,20 @@ def __str__(self):
 
 def number_of_likes(self):
     return self.likes.count()
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['-created_on']
+
+
+def __str__(self):
+    return f'Comment {self.body} by {self.name}'
